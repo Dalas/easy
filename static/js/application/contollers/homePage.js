8cecf8228e07570214f0ec0045a517e8772b7b82
yura <yurkinnnn@gmail.com>
2016-09-14 21:02:37 +0300

    Add jQ and Bootstrap3

--- static/js/application/contollers/homePage.js
+++ static/js/application/contollers/homePage.js
@@ -1,5 +1,5 @@
 define(["angular"],
     function(ng) {
-        console.log(ng)
+        console.log(ng);
     }
 );
--- static/js/main.js
+++ static/js/main.js
@@ -5,10 +5,15 @@ require.config({
     shim: {
         "angular": {
             exports: 'angular'
+        },
+        "bootstrap": {
+            deps: ['jquery']
         }
     },
     paths: {
         "angular": "bower_components/angular/angular",
+        "jquery": "bower_components/jquery/dist/jquery.min",
+        "bootstrap": "bower_components/bootstrap/dist/js/bootstrap.min",
 
         // application
         "homePage": "application/contollers/homePage"
